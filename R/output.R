0ef427e5b830ef24903d2ed1e162e918859ed5a6
john <bblodfon@gmail.com>
2019-08-13 16:44:50 +0200

    small refactoring for 'print_biomarkers_per_predicted_synergy' + doc

--- R/output.R
+++ R/output.R
@@ -21,18 +21,33 @@ print_model_and_drug_stats =
     pretty_print_string(paste("Number of nodes:", nodes), with.gt = FALSE)
 }
 
-#' yeah baby
+#' Print biomarkers for each predicted synergy
+#'
+#' @param biomarkers.dir string. It specifies the full path name of the
+#' directory which holds the biomarker files for each drug combination in the
+#' \code{predicted.synergies}. The biomarker files must be formatted as:
+#' \emph{\%drug.comb\%_biomarkers_active} or
+#' \emph{\%drug.comb\%_biomarkers_inhibited}, where \%drug.comb\% is an element of
+#' the \code{predicted.synergies} vector. If the files are not properly formatted
+#' or don't even exist, zero biomarkers are reported.
+#' @param predicted.synergies a character vector of the synergies (drug
+#' combination names) that were predicted by \strong{at least one} of the models
+#' in the dataset.
+#' @param html.output logical. If TRUE, it makes the printed output nice for
+#' an HTML document. Default value: TRUE.
 #'
 #' @importFrom usefun pretty_print_string print_empty_line pretty_print_vector_values pretty_print_bold_string
 #' @importFrom utils read.table
+#' @export
 print_biomarkers_per_predicted_synergy =
-  function(biomarkers.dir, drug.comb, predicted.synergies, html.output = TRUE) {
+  function(biomarkers.dir, predicted.synergies, html.output = TRUE) {
     pretty_print_string("")
     for (drug.comb in predicted.synergies) {
       # get the active biomarkers
       active.biomarkers.file =
         paste0(biomarkers.dir, drug.comb, "_biomarkers_active")
-      if (file.size(active.biomarkers.file) == 0) {
+      if (file.size(active.biomarkers.file) == 0
+          || !file.exists(active.biomarkers.file)) {
         biomarkers.active.names = NULL
       } else {
         biomarkers.active =
@@ -43,7 +58,8 @@ print_biomarkers_per_predicted_synergy =
       # get the inhibited biomarkers
       inhibited.biomarkers.file =
         paste0(biomarkers.dir, drug.comb, "_biomarkers_inhibited")
-      if (file.size(inhibited.biomarkers.file) == 0) {
+      if (file.size(inhibited.biomarkers.file) == 0
+          || !file.exists(inhibited.biomarkers.file)) {
         biomarkers.inhibited.names = NULL
       } else {
         biomarkers.inhibited =
