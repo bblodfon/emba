5bf38a977be67c65eba09e71db275f235ba5c043
john <bblodfon@gmail.com>
2019-08-14 18:49:14 +0200

    added documentation about the packge itself

---
+++ R/emba.R
@@ -0,0 +1,10 @@
+#' emba
+#'
+#' A collection of useful functions that help the analysis and discovery of
+#' biomarkers in an ensemble boolean model dataset
+#'
+#' For a complete list of functions, use \code{library(help = "emba")}
+#'
+#' @docType package
+#' @name emba
+NULL
