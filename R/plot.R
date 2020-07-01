e233b436d0c2673569d804f940de8d15cc1726f0
john <bblodfon@gmail.com>
2020-07-01 05:14:25 +0200

    add correct function link

--- R/plot.R
+++ R/plot.R
@@ -289,7 +289,7 @@ plot_avg_state_diff_graph_vis = function(net, diff, nodes.size = 20, title) {
 
 #' Plot the graph of average state differences (igraph)
 #'
-#' This function uses the \code{\link[igraph]{igraph}} package to plot a network
+#' This function uses the \code{\link[igraph]{plot.igraph}} package to plot a network
 #' of nodes. The nodes are positioned according to the specified coordinates
 #' given by the \code{layout} parameter and the colors are derived using the
 #' \code{diff} values and the \code{\link{get_node_colors}} function. The color
